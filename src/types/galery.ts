b96240ea803bf843a46e9cb30f0f54627217ffa4
Yudiel Chala <dgtlly.literate@gmail.com>
2025-08-14 11:36:46 -0500

    Updated the order in gallery items

--- src/types/galery.ts
+++ src/types/galery.ts
@@ -2,8 +2,8 @@ export interface GaleryItem {
   id: number;
   imageUrl: string;
   name: string;
-  year: string;
   description: string;
+  year: string;
   technique: string;
   dimensions: string;
 }
