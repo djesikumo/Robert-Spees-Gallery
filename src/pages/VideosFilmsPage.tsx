cd06b85688610a7d835b9e3b9e40ef9ddcbb8931
Daniel <izaguirredaniel9712@gmail.com>
2025-08-28 14:56:28 -0400

    Italic off

--- src/pages/VideosFilmsPage.tsx
+++ src/pages/VideosFilmsPage.tsx
@@ -6,7 +6,7 @@ const VideosFilmsPage = () => {
       <div className="flex flex-col items-center">
         <h1 className="text-center sm:text-start text-6xl font-bold mb-8 text-gray-800">Videos & Films</h1>
         <img src={Film} className="w-96 rounded-lg mb-4" />
-        <p className="w-96 text-gray-800 italic text-center mb-4">“Lost In The Wash” is a new experimental feature film by Rbt. Sps. currently still in production. Tyler J. Ladd stars alongside Sps. himself in this eclectic travelogue.</p>
+        <p className="w-96 text-gray-800 text-center mb-4"><span className='italic'>“Lost In The Wash”</span> is a new experimental feature film by Rbt. Sps. currently still in production. Tyler J. Ladd stars alongside Sps. himself in this eclectic travelogue.</p>
         <p className="w-96 text-gray-800 text-center">Director of Photography: Jeremiah M. Carter. Associate Producer: Jeffrey C. Stanley. The film’s tentative completion date is October 2026.</p>
       </div>
     </main>
